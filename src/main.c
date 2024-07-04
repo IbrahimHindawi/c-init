2c95801c62bc74510a08a0084ef1c66d3c4e18bf
IbrahimHindawi <ihindawi@gmail.com>
2024-07-04 22:51:44 +0400

    updated Component naming style to fit HK, added new examples to main, synced haikal

--- src/main.c
+++ src/main.c
@@ -5,46 +5,62 @@
 #define HKARRAY_IMPL
 #include <hkArray.h>
 
-// #define HKLIST_IMPL
-// #include <hkList.h>
+#define HKNODE_IMPL
+#include <hkNode.h>
+
+#define HKLIST_IMPL
+#include <hkList.h>
 
 // #include "Component.h"
 
 int main(int argc, char *argv[]) {
     //haikal@hkArray:i8
-    hkArray_i8 string = hkArray_i8_create(10);
+    hkArray_i8 string = hkArray_i8_create(27);
     for (int i = 0; i < string.length; ++i) {
         string.data[i] = 0b01100000 | i + 1;
     }
     string.data[string.length - 1] = '\0';
-    printf("%s\n", string.data);
+    printf("string: %s\n", string.data);
     hkArray_i8_destroy(&string);
 
     //haikal@hkArray:vec3
-    hkArray_vec3 vectors = hkArray_vec3_create(21);
-    for (int i = 0; i < vectors.length; ++i) {
-        vectors.data[i].x = 1.0f;
-        vectors.data[i].y = 0.0f;
-        vectors.data[i].z = 3.141592f;
+    hkArray_vec3 *vectors = malloc(sizeof(hkArray_vec3));
+    *vectors = hkArray_vec3_create(21);
+    for (int i = 0; i < vectors->length; ++i) {
+        vectors->data[i].x = 1.0f;
+        vectors->data[i].y = (f32)i;
+        vectors->data[i].z = 3.141592f;
     }
-    for (int i = 0; i < vectors.length; ++i) {
-        printf("vector[%d] = {%f, %f, %f}\n", 
-            i,
-            vectors.data[i].x,
-            vectors.data[i].y,
-            vectors.data[i].z);
+    for (int i = 0; i < vectors->length; ++i) {
+        printf("vectors[%d] = {%f, %f, %f}\n", i, vectors->data[i].x, vectors->data[i].y, vectors->data[i].z);
     }
-    hkArray_vec3_destroy(&vectors);
-    //haikal@hkArray:f32
-
-    // Component comp = componentCreate(10);
-    // printf("comp.id = %d\n", comp.id);
-    // componentDestroy(&comp);
+    hkArray_vec3_destroy(vectors);
 
     //haikal@hkArray:i8
     hkArray_i8 arr = hkArray_i8_create(8);
     hkArray_i8_destroy(&arr);
-    printf("what");
+
+    //haikal@hkNode:i32
+    //haikal@hkList:i32
+    hkList_i32 *loi = hkList_i32_create();
+    hkList_i32_append(loi, 0);
+    hkList_i32_append(loi, 1);
+    hkList_i32_append(loi, 2);
+    hkList_i32_append(loi, 3);
+    hkNode_i32 *iter = loi->head;
+    while (iter != NULL) {
+        printf("loi: {%d, %p}\n", iter->data, iter->next);
+        iter = iter->next;
+    }
+    hkList_i32_destroy(&loi);
+
+    // for this to work, we need to read all the included files
+    // compile_commands.json should be enough...
+    // or, use a unity build and just include everything in main.c 
+    // but LSP will die in Component.h...
+    // Component comp = Component_create(10);
+    // printf("comp.id = %d\n", comp.id);
+    // Component_destroy(&comp);
 
     return 0;
 }
